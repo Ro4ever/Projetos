e40bc428b9a6046393d961ee4c8d0263d960b3f4
Ro4ever <robertoellmer@gmail.com>
2023-06-12 18:47:29 -0300

    v1.0

--- src/matrix/desafio08.java
+++ src/matrix/desafio08.java
@@ -8,7 +8,7 @@ public class desafio08 {
 
         double[][] matrix = new double[4][4];
         int valorInformado1;
-        int valorInformado2;
+        int valorInformado2 = 0;
         double [] primaria = new double[4];
         double [] secundaria = new double[4];
 
@@ -30,35 +30,41 @@ public class desafio08 {
         }
         System.out.println("Informe o um valor para divisão da diagonal primária: ");
         valorInformado1 = scan.nextInt();
-
-        System.out.println("Informe o um valor para multiplicação da diagonal secundária: ");
-        valorInformado2 = scan.nextInt();
-
-        for(int i=0; i<4; i++)
+        if(valorInformado1 == 0)
         {
-            for(int j=0; j<4; j++)
+            System.out.println("Não pode realizar divisões com zero.");
+        }
+        else
+        {
+            System.out.println("Informe o um valor para multiplicação da diagonal secundária: ");
+            valorInformado2 = scan.nextInt();
+
+            for(int i=0; i<4; i++)
             {
-                primaria[i] = matrix[i][j] / valorInformado1;
-                i++;
+                for(int j=0; j<4; j++)
+                {
+                    primaria[i] = matrix[i][j] / valorInformado1;
+                    i++;
+                }
+            }
+            for(int i=0; i<4; i++)
+            {
+                for(int j=0; j<4; j++)
+                {
+                    secundaria[i] = matrix[i][3 - i] * valorInformado2;
+                }
+            }
+            System.out.println("Valores da divisão primária: ");
+            for(int i=0; i<4; i++)
+            {
+                System.out.print(primaria[i] + " ");
+            }
+            System.out.println("\nValores da multiplicação secundária: ");
+            for(int i=0; i<4; i++)
+            {
+                System.out.print(secundaria[i] + " ");
             }
         }
-        for(int i=0; i<4; i++)
-        {
-            for(int j=0; j<4; j++)
-            {
-                secundaria[i] = matrix[i][4 - 1 - i] * valorInformado2;
-                i++;
-            }
-        }
-        System.out.println("Valores da divisão primária: ");
-        for(int i=0; i<4; i++)
-        {
-            System.out.print(primaria[i] + " ");
-        }
-        System.out.println("\nValores da multiplicação secundária: ");
-        for(int i=0; i<4; i++)
-        {
-            System.out.print(secundaria[i] + " ");
-        }
+
     }
 }
